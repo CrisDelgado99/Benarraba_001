f79cd17e16763201b4aae9c02a5896b24c6be113
CrisDelgado99 <125089261+CrisDelgado99@users.noreply.github.com>
2024-12-15 18:36:44 +0100

    Build_1.0.3
    I changed the quantity of ammo

--- Assets/Scripts/PowerUpController.cs
+++ Assets/Scripts/PowerUpController.cs
@@ -54,7 +54,7 @@ public class PowerUpController : MonoBehaviour
             else if (powerUpType == PowerUpType.AmmoBox)
             {
                 WeaponController weaponController = other.GetComponent<WeaponController>();
-                weaponController.CurrentAmmo += Random.Range(6, 11);
+                weaponController.CurrentAmmo += Random.Range(5, 9);
                 AudioManager.Instance.PlayGetAmmoAudio();
                 Destroy(gameObject);
             }
